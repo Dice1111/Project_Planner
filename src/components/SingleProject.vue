5092ea5513f2684ee30096d9c7e495088119b992
Dice1111 <kingdice1111@gmail.com>
2022-03-26 18:14:30 +0630

    bordercolor_final

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -11,7 +11,7 @@
                 <span class="material-icons">
                     edit
                 </span>
-                <span class="material-icons">
+                <span class="material-icons" @click="completeProject">
                     done
                 </span>   
             </div>
@@ -41,6 +41,27 @@ export default {
             .catch(()=>{
                 console.log(err);
             })
+        },
+        completeProject(){
+            let updateCompleteRoute=this.api+"/"+this.project.id;
+            fetch(updateCompleteRoute,{
+                method:"PATCH",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(
+                    {
+                        complete:!this.project.complete
+                    }
+                )
+                })
+            .then(()=>{
+                this.$emit("complete",this.project.id)
+            })
+            .catch(()=>{
+                console.log(err);
+            })
+                
         }
     }
 }
